e79ffaca3b54367c61b46055ddfc6a725510d132
Adnit Kamberi <adnitkamberi10@gmail.com>
2020-03-28 08:36:59 +0100

    added func currentDT to get current DT from system

--- Projekti 2/kerkesa3.cpp
+++ Projekti 2/kerkesa3.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <sstream>
 #include <iomanip>
+#include <time.h>
 using namespace std;
 
 class Data
@@ -63,14 +64,41 @@ public:
     stringstream gypiS;
     gypiS << setw(2) << setfill('0') << dita() << "/";
     gypiS << setw(2) << setfill('0') << muaji() << "/";
-    gypiS << viti();
+    gypiS << viti() << endl;
     return gypiS.str();
   }
 };
 
+Data currentDT()
+{
+
+  time_t timer;
+  time(&timer);
+
+  int year, month, day, dDuration, mDuration, yDuration;
+
+  // gjatsia e dites n sekonda
+  dDuration = 60 * 60 * 24;
+  // gjatsia e muajit n sekonda
+  mDuration = dDuration * 30;
+  // gjatsia e vitit n sekonda
+  yDuration = dDuration * 365.24;
+
+  year = 1970 + (timer / yDuration);
+  month = ((timer % yDuration) / mDuration) + 1;
+  day = (((timer % yDuration) % mDuration) / dDuration) + 1;
+
+  Data current = Data(day, month, year);
+
+  return current;
+}
+
 int main()
 {
+
   Data *ditlindja = new Data(27, 7, 2001);
+  Data Tani = currentDT();
+  cout << Tani.shtyp();
   cout << "Tremujori:  " << ditlindja->tremujori() << endl;
   cout << "Shekulli : " << ditlindja->shekulli() << endl;
   cout << "Ditlindja : " << ditlindja->shtyp() << endl;
